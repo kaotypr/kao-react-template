aacf55fdc1b31ba17b313b947bdb168763e48a22
adhityasan <adhityad3v@gmail.com>
2019-10-18 23:42:39 +0700

    localstorage constants

---
+++ src/constants/localstorage.js
@@ -0,0 +1,8 @@
+export const DEFAULT_INITIATED = {
+  app_name: 'kao-project.kao-fe',
+  last_visit: new Date().getTime(),
+};
+
+export const DEFAULT_NOTIFICATION = {
+  client: 'generated_client_uid',
+};
